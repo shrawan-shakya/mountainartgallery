b5a154f53c8b7c21c22b195b0b81bcf6a25e6d46
Shrawan Shakya <gdesignershrawan@gmail.com>
2025-07-04 20:00:07 +1000

    Update dashboard and artwork features

--- src/firebase.js
+++ src/firebase.js
@@ -7,10 +7,10 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKHC5ZiLgtuIIKBD95CuMfUt_S-HAErAA",
+ apiKey: "AIzaSyAKHC5ZiLgtuIIKBD95CuMfUt_S-HAErAA",
   authDomain: "mountainartgallery-70f52.firebaseapp.com",
   projectId: "mountainartgallery-70f52",
-  storageBucket: "mountainartgallery-70f52.appspot.com",
+  storageBucket: "mountainartgallery-70f52.firebasestorage.app",  // <-- updated here
   messagingSenderId: "27490042984",
   appId: "1:27490042984:web:fbf616fd55a0ca3c0fe7dd"
 };

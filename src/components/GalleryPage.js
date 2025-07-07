35c5848c0f450d5542fbcd0baf77a355c202659f
Shrawan Shakya <gdesignershrawan@gmail.com>
2025-07-07 15:22:06 +1000

    Update dashboard and artwork features

--- src/components/GalleryPage.js
+++ src/components/GalleryPage.js
@@ -4,15 +4,7 @@ import Gallery from './Gallery';
 
 export default function GalleryPage() {
   return (
-    <div style={{ paddingTop: '3rem' }}>
-      <h2 style={{
-        fontSize: '2rem',
-        fontWeight: 700,
-        textAlign: 'center',
-        marginBottom: '2rem'
-      }}>
-        All Paintings
-      </h2>
+    <div>
       <Gallery />
     </div>
   );

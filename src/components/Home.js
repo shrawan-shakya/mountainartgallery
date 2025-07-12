5e4f6f384567b0fda06eb32cd010fbdb9e258c17
Shrawan Shakya <gdesignershrawan@gmail.com>
2025-07-12 20:40:52 +1000

    Initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,40 @@ import FeaturedArtworks from './FeaturedArtworks';
 import ContactForm from './ContactForm';
 import PhotographyToPainting from './PhotographyToPainting';
 import '../styles/Home.css';
+import heroMobile from '../assets/hero-mobile.jpg'; // Ensure this image exists
 
 export default function Home() {
   return (
     <>
       <header className="home-header">
+        {/* Mobile Hero Image */}
+        <img src={heroMobile} alt="Art Hero" className="hero-image-mobile" />
+
         <div className="hero-content">
-          <h1>
-            Shop our selection of <br />
-            one‑of‑a‑kind paintings.
-          </h1>
+          {/* Desktop Hero Text */}
+          <div className="hero-text-desktop">
+            <h1>
+              Shop our selection of <br />
+              one‑of‑a‑kind paintings.
+            </h1>
 
-          <p className="subtitle">
-            Our talented artists create beautiful, meaningful paintings that are
-            sure to be treasured for years to come.
-          </p>
+            <p className="subtitle">
+              Our talented artists create beautiful, meaningful paintings that are
+              sure to be treasured for years to come.
+            </p>
 
-          <p className="subtitle">
-            • 1000s of paintings&nbsp;&nbsp;• 20&nbsp;years of experience
-          </p>
+            <p className="subtitle">
+              • 1000s of paintings&nbsp;&nbsp;• 20&nbsp;years of experience
+            </p>
+          </div>
+
+          {/* Mobile Hero Text */}
+          <div className="hero-text-mobile">
+            <h1>Find Your Next Masterpiece</h1>
+            <p className="subtitle">Thousands of stunning paintings created by passionate artists from Nepal.
+
+</p>
+          </div>
 
           <Link to="/gallery" className="explore-button">
             Shop Gallery
